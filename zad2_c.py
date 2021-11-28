a826a598c37538188e3f9e9674f3912c311ff76e
Kajetan Gęgotek <gogo.gegotek@gmail.com>
2021-11-28 08:38:34 +0100

    pep 8

--- zad2_c.py
+++ zad2_c.py
@@ -7,9 +7,10 @@ def for_with_range() -> None:
         Does not return anything
     """
     # prints every element divisible by 2
-    for i in range(0,10):
-        if i%2 == 0:
+    for i in range(0, 10):
+        if i % 2 == 0:
             print(i)
+
 
 if __name__ == "__main__":
     for_with_range()
