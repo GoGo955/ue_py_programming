3d9b5cbf10ba84bab76f9c03a4b80a65f7e0541e
Kajetan Gęgotek <gogo.gegotek@gmail.com>
2021-11-28 08:44:39 +0100

    pep8

--- zad2.py
+++ zad2.py
@@ -8,12 +8,14 @@ class Library:
         self.phone = phone
 
     def __str__(self):
-        return f"library, which located in {self.city} (zip code - {self.zip_code}), at {self.street}. Library is open {self.open_hours}, and the contact number is {self.phone}"
+        return f"library, which located in {self.city} (zip code - {self.zip_code}), at {self.street}. " \
+               f"Library is open {self.open_hours}, and the contact number is {self.phone}"
+
 
 class Employee:
 
     def __init__(
-            self, first_name: str, 
+            self, first_name: str,
             last_name: str,
             hire_date: str,
             birth_date: str,
@@ -21,8 +23,7 @@ class Employee:
             street: str,
             zip_code: str,
             phone: str
-        ):
-
+    ):
         self.first_name = first_name
         self.last_name = last_name
         self.hire_date = hire_date
@@ -33,19 +34,20 @@ class Employee:
         self.phone = phone
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, from {self.city} ({self.zip_code}), {self.street} and born on {self.birth_date}. Hired on {self.hire_date}. Phone num is {self.phone}"
+        return f"{self.first_name} {self.last_name}, from {self.city} ({self.zip_code}), {self.street} and born on " \
+               f"{self.birth_date}. Hired on {self.hire_date}. Phone num is {self.phone}"
 
 
 class Book:
 
     def __init__(
-            self, 
-            library: Library, 
-            publication_date: str, 
-            author_name: str, 
-            author_surname: str, 
+            self,
+            library: Library,
+            publication_date: str,
+            author_name: str,
+            author_surname: str,
             number_of_pages: int
-        ):
+    ):
         self.library = library
         self.publication_date = publication_date
         self.author_name = author_name
@@ -53,24 +55,27 @@ class Book:
         self.number_of_pages = number_of_pages
 
     def __str__(self):
-        return f"{self.author_name} {self.author_surname} book from {self.publication_date} and with {self.number_of_pages} pages. It's in {self.library}"
+        return f"{self.author_name} {self.author_surname} book from {self.publication_date} and with " \
+               f"{self.number_of_pages} pages. It's in {self.library}"
+
 
 class Order:
 
     def __init__(
-            self, 
-            employee: Employee, 
-            student: str,  
-            books: Book, 
+            self,
+            employee: Employee,
+            student: str,
+            books: Book,
             order_date: str
-        ):
-            self.employee = employee
-            self.student = student
-            self.books = books
-            self.order_date = order_date
+    ):
+        self.employee = employee
+        self.student = student
+        self.books = books
+        self.order_date = order_date
 
     def __str__(self):
-        return f'This is {self.student}s order from {self.order_date}. He/she order {self.books}. Order will be handled by {self.employee}'
+        return f'This is {self.student}s order from {self.order_date}. He/she order {self.books}. ' \
+               f'Order will be handled by {self.employee}'
 
 
 if __name__ == "__main__":
@@ -83,37 +88,36 @@ if __name__ == "__main__":
     book4 = Book(lib2, "12-11-1245", "Wan", "Wan", 155)
     book5 = Book(lib2, "12-11-1245", "San", "San", 166)
 
-
     employee1 = Employee(
-        'bob', 
-        'ross', 
-        '10-10-2021', 
-        '10-10-2020', 
-        'NY', 
-        'long street', 
-        '43-425', 
+        'bob',
+        'ross',
+        '10-10-2021',
+        '10-10-2020',
+        'NY',
+        'long street',
+        '43-425',
         '324-324-55'
     )
 
     employee2 = Employee(
-        'Roob', 
-        'BOss', 
-        '10-10-2021', 
-        '10-10-2020', 
-        'NY', 
-        'mid street', 
-        '41-425', 
+        'Roob',
+        'BOss',
+        '10-10-2021',
+        '10-10-2020',
+        'NY',
+        'mid street',
+        '41-425',
         '324-324-55'
     )
 
     employee3 = Employee(
-        'SOB', 
-        'Soss', 
-        '10-10-2021', 
-        '10-10-2020', 
-        'NY', 
-        'short street', 
-        '42-425', 
+        'SOB',
+        'Soss',
+        '10-10-2021',
+        '10-10-2020',
+        'NY',
+        'short street',
+        '42-425',
         '421-324-55'
     )
 
--- zad3.py
+++ zad3.py
@@ -9,23 +9,26 @@ class Property:
     def __str__(self):
         return f"This {self.area} m2, with {self.rooms} rooms property is at {self.address} and it's for {self.price}$."
 
+
 class House(Property):
 
-    def __init__(self, area: int, rooms: int, price: int, address: str, plot:int):
+    def __init__(self, area: int, rooms: int, price: int, address: str, plot: int):
         super().__init__(area, rooms, price, address)
         self.plot = plot
 
     def __str__(self):
         return super().__str__() + f" It has a plot of {self.plot} m2."
 
+
 class Flat(Property):
 
     def __init__(self, area: int, rooms: int, price: int, address: str, floor: int):
         super().__init__(area, rooms, price, address)
         self.floor = floor
-    
+
     def __str__(self):
         return super().__str__() + f" It is at {self.floor} floors."
+
 
 if __name__ == "__main__":
     house = House(120, 6, 1240000, 'super street', 1000)
