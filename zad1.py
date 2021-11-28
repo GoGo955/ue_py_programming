1730313a23d0d29b3e772d90c39c3e70f84820a8
Kajetan Gęgotek <gogo.gegotek@gmail.com>
2021-11-28 09:45:37 +0100

    pep8 p3

--- zad1.py
+++ zad1.py
@@ -1,11 +1,10 @@
 class Student:
-
     def __init__(self, name: str, marks: int):
         self.name = name
         self.marks = marks
 
     def __str__(self):
-        return f'Student {self.name} with mark {self.marks}'
+        return f"Student {self.name} with mark {self.marks}"
 
     def is_passed(self) -> bool:
         return self.marks > 50
@@ -15,5 +14,5 @@ if __name__ == "__main__":
     stud1 = Student("Bob Ross", 74)
     stud2 = Student("Rob Boss", 35)
 
-    print(f'{stud1}. Did he pass? -> {stud1.is_passed()}')
-    print(f'{stud2}. Did he pass? -> {stud2.is_passed()}')
+    print(f"{stud1}. Did he pass? -> {stud1.is_passed()}")
+    print(f"{stud2}. Did he pass? -> {stud2.is_passed()}")
--- zad3.py
+++ zad3.py
@@ -1,5 +1,4 @@
 class Property:
-
     def __init__(self, area: int, rooms: int, price: int, address: str):
         self.area = area
         self.rooms = rooms
@@ -7,11 +6,11 @@ class Property:
         self.address = address
 
     def __str__(self):
-        return f"This {self.area} m2, with {self.rooms} rooms property is at {self.address} and it's for {self.price}$."
+        return f"This {self.area} m2, with {self.rooms} rooms property is at {self.address} " \
+               f"and it's for {self.price}$."
 
 
 class House(Property):
-
     def __init__(self, area: int, rooms: int, price: int, address: str, plot: int):
         super().__init__(area, rooms, price, address)
         self.plot = plot
@@ -21,7 +20,6 @@ class House(Property):
 
 
 class Flat(Property):
-
     def __init__(self, area: int, rooms: int, price: int, address: str, floor: int):
         super().__init__(area, rooms, price, address)
         self.floor = floor
@@ -31,8 +29,8 @@ class Flat(Property):
 
 
 if __name__ == "__main__":
-    house = House(120, 6, 1240000, 'super street', 1000)
-    flat = Flat(80, 4, 250000, 'north streer', 6)
+    house = House(120, 6, 1240000, "super street", 1000)
+    flat = Flat(80, 4, 250000, "north streer", 6)
 
     print(house)
     print(flat)
