43fa0be9b5def7bdd3c491e712025dc1fb1d829e
GoGo955 <77108321+GoGo955@users.noreply.github.com>
2021-12-19 14:39:39 +0100

    lab_10

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 Helper module containing used functions
 """
 import glob
+import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -15,8 +16,22 @@ def read_text_from_image(path: str) -> str:
     Returns:
         string from image
     """
-    return pytesseract.image_to_string(path)
+    conv_img = clean_img(path)
+    return pytesseract.image_to_string(conv_img)
 
+
+def clean_img(path: str):
+    """
+    docstr
+    """
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    # img_converted = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # img_converted = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # img_converted = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # img_converted = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    # img_converted = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    img_converted = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    return img_converted
 
 def print_images_text() -> None:
     """
