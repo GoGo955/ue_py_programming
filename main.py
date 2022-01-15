dd8cae2a98284b4d57b6e61bf98d56262b7c4786
GoGo955 <77108321+GoGo955@users.noreply.github.com>
2022-01-15 11:14:59 +0100

    s

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-import cv2
+from img_rec import img_rec
 
-# Load a model imported from Tensorflow
-tensorflowNet = cv2.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
-
-# Input image
-img = cv2.imread('pics/cctv.jpg')
-rows, cols, _ = img.shape
-
-# Use the given image as input
-tensorflowNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-
-# Runs a forward pass to compute the net output
-networkOutput = tensorflowNet.forward()
-
-# Loop on the outputs
-for detection in networkOutput[0, 0, :, :]:
-
-    score = float(detection[2])
-    if score > 0.4:
-
-        left = detection[3] * cols
-        top = detection[4] * rows
-        right = detection[5] * cols
-        bottom = detection[6] * rows
-
-        # draw a red rectangle around detected objects
-        cv2.rectangle(
-            img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2
-        )
-
-# Show the image with a rectagle surrounding the detected objects
-cv2.imshow('Image', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    img_rec("cctv")
